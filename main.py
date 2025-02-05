b7df63d6ba18389593593ecd592e23a6aaa0228a
regulad <>
2025-02-04 19:16:13 -0500

    Init

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from __future__ import annotations
 
-from selenium.webdriver import Chrome, ChromeOptions
+from selenium.webdriver import Chrome, ChromeOptions, ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from urllib.parse import urlparse
 from time import sleep
 from replit import db
+from threading import Thread
+from uuid import uuid4
+#import pyautogui
 
 from localstorage import LocalStorage
 
 # change this to a website like discord.com to enable LocalStorage
-SINGLE_PAGE = ""
-
+SINGLE_PAGE = False#"https://connor33341.github.io/static/virtualkeyboard/iframe/index.html?src=https://wikipedia.org"
+STARTING_PAGE = "https://duckduckgo.com"
+TARGET_ELEMENT = "q"
+KEYS = str(input("Keys: "))
 
 def is_cookies() -> None:
     return len([key for key in db.keys() if key.isnumeric()]) > 0
@@ -82,7 +88,14 @@ def load_localstorage(ls: LocalStorage) -> None:
     else:
         print("done")
 
-
+def rec():
+    while True:
+        ID = str(uuid4())
+        Url = driver.current_url
+        Url.removeprefix("https://")
+        Url.replace("/","-")
+        Url.replace(".","-")
+        #driver.save_screenshot(f"rec/{ID}.png")
 if __name__ == "__main__":
     print("Starting Ultimate Chrome 2...")
 
@@ -111,14 +124,46 @@ if __name__ == "__main__":
         load_localstorage(ls)
 
     if not SINGLE_PAGE:
-        driver.get("https://uc-2-landing-regulad.replit.app/")
+        #old: "https://uc-2-landing-regulad.replit.app/"
+        driver.get(STARTING_PAGE)
+        SearchBox = driver.find_element(By.NAME, TARGET_ELEMENT)
+        SearchBox.send_keys(KEYS+Keys.RETURN)
     
     print("Done loading! Stop the repl to save cookies.")
 
     try:
-        while True:
-            sleep(60)
-            save_cookies(driver)
+        RecThread = Thread(target=rec)
+        RecThread.start()
+        Running = True
+        while Running:
+            sleep(0.1)
+            #save_cookies(driver)
+            try:
+                Command = input("Text: ")
+                if Command == "<ENTER>":
+                    Command = Keys.RETURN
+                if Command == "<TAB>":
+                    Command = Keys.TAB
+                if Command == "<CTRL>":
+                    Command = Keys.CONTROL
+                if Command == "<SHIFT>":
+                    Command = Keys.SHIFT
+                if Command == "<ESC>":
+                    Command = Keys.ESCAPE
+                if Command == "<BACKSPACE>":
+                    Command = Keys.BACKSPACE
+                if Command == "<DEL>":
+                    Command = Keys.BACKSPACE
+                if Command == "<END>":
+                    Running = False
+                    break
+                AC = ActionChains(driver)
+                AC.send_keys(Command)
+                AC.perform()
+                #pyautogui.click()
+                #pyautogui.typewrite(Command)
+            except Exception as Error:
+                print(Error)
             if SINGLE_PAGE:
                 save_localstorage(ls)
     except KeyboardInterrupt:
